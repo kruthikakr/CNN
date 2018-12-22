7869d208bf146bf1c196809ac226af301e8a9c1b
Pavan Kumar Polavarapu <pavan.vnr@gmail.com>
2018-12-22 17:40:47 -0500

    005 - Implemented Softmax Loss

--- 002-ImageClassification/assignment1/cs231n/classifiers/softmax.py
+++ 002-ImageClassification/assignment1/cs231n/classifiers/softmax.py
@@ -1,62 +1,96 @@
 import numpy as np
 from random import shuffle
 
+
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    """
+    Softmax loss function, naive implementation (with loops)
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    # Setting up variables
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    for i in range(num_train):
+        score = X[i].dot(W)
+        score -= np.max(score)
+        total = np.sum(np.exp(score))
 
-  return loss, dW
+        p = lambda j: np.exp(score[j]) / total
+        loss += -np.log(p(y[i]))
+
+        for n in range(num_classes):
+            probability_current = p(n)
+            #For True score we have to substract X[i]
+            #Since sum of probabilities is 1, we can substract 1 for true class
+            dW[:, n] += (probability_current - (n == y[i])) * X[i,:]
+
+    loss /= num_train
+    dW /= num_train
+
+    # Adding Regularization loss and applying learning rate
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+    
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    # Setting up variables
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
 
-  return loss, dW
+    scores = X.dot(W)
+    scores -= np.max(scores, axis=1, keepdims=True)
+    totals = np.sum(np.exp(scores), axis=1, keepdims=True)
+    probability = np.exp(scores) / totals
+    loss = np.sum(-np.log(probability[np.arange(num_train),y])) / num_train
 
+    probability[np.arange(num_train), y] = -1
+    dW = X.T.dot(probability) / num_train
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
